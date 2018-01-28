52dc2df450f03e4255c7156fa93a0afa600def92
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-28 18:34:54 -0500

    Display animated GIF

--- GiphyViewer/Controllers/DetailViewController.m
+++ GiphyViewer/Controllers/DetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "DetailViewController.h"
+#import "Giphy.h"
 
 @interface DetailViewController ()
 
@@ -16,22 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    if (self.giphy) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.giphy.animatedURL];
+        [self.webView loadRequest:request];
+    }
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
