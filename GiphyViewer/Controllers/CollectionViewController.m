4344f7d9fd65394c2452eb52f71fb6576f8207f6
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-28 18:32:04 -0500

    Perform segue on selection

--- GiphyViewer/Controllers/CollectionViewController.m
+++ GiphyViewer/Controllers/CollectionViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CollectionViewController.h"
+#import "DetailViewController.h"
 #import "CollectionViewCell.h"
 #import "Giphy.h"
 
@@ -48,15 +49,17 @@ static NSString * const reuseIdentifier = @"GIFViewerCell";
     [task resume];
 }
 
-/*
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"showDetail"]) {
+        NSIndexPath *selectedPath = [self.collectionView indexPathsForSelectedItems][0];
+        
+        Giphy *giphy = self.giphys[selectedPath.row];
+        DetailViewController *detailViewController = segue.destinationViewController;
+        detailViewController.giphy = giphy;
+    }
 }
-*/
 
 #pragma mark <UICollectionViewDataSource>
 
@@ -78,33 +81,8 @@ static NSString * const reuseIdentifier = @"GIFViewerCell";
 
 #pragma mark <UICollectionViewDelegate>
 
-/*
-// Uncomment this method to specify if the specified item should be highlighted during tracking
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
-	return YES;
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    [self performSegueWithIdentifier:@"showDetail" sender:self];
 }
-*/
-
-/*
-// Uncomment this method to specify if the specified item should be selected
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
-}
-*/
-
-/*
-// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
-	return NO;
-}
-
-- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	return NO;
-}
-
-- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	
-}
-*/
 
 @end
