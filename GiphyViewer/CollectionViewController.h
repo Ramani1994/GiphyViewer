957e385954dea665b5f91082d07ff616c0940a96
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-28 14:57:07 -0500

    Include custom class for collection view

---
+++ GiphyViewer/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  GiphyViewer
+//
+//  Created by Mohammed Al-Dahleh on 2018-01-28.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
