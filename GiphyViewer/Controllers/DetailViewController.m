a9c50190c9576339331ffc07397272597b76e723
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-01-28 18:59:17 -0500

    Swipe to dimiss detail view

--- GiphyViewer/Controllers/DetailViewController.m
+++ GiphyViewer/Controllers/DetailViewController.m
@@ -35,6 +35,18 @@
                                        action:@selector(dismiss)];
     dismiss.numberOfTapsRequired = 1;
     [self.view addGestureRecognizer:dismiss];
+    
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeDismiss)];
+    swipe.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:swipe];
+}
+
+- (void)swipeDismiss {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view.transform = CGAffineTransformMakeTranslation(400, 0);
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:true completion:nil];
+    }];
 }
 
 - (void)dismiss {
